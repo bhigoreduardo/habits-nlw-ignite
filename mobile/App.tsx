ac90fb737efc7da06e5ab0fc464526efbc53d24e
bhigoreduardo <bhigoreduardo@gmail.com>
2023-03-23 09:20:29 -0300

    Created mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+    </>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
