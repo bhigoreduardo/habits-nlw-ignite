339eb14d6dbdf7b83ced8827d1a257b4e32e6296
bhigoreduardo <bhigoreduardo@gmail.com>
2023-03-23 16:16:06 -0300

    Added logo in app

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native'
 
+import Logo from '../assets/logo.svg'
+
 export function Home() {
   return (
     <View className="flex-1 bg-background">
+      <Logo />
       <Text className="text-zinc-50">Home</Text>
     </View>
   )
