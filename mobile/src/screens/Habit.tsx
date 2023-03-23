8378d6ba766f69297d76688c73c2e9ef382f6e37
bhigoreduardo <bhigoreduardo@gmail.com>
2023-03-23 08:56:29 -0300

    Deleted all mobile app

--- mobile/src/screens/Habit.tsx
+++
@@ -1,7 +0,0 @@
-import { View } from 'react-native'
-
-export function Habit() {
-  return (
-    <View>Habit</View>
-  )
-}
--- mobile/src/screens/New.tsx
+++
@@ -1,7 +0,0 @@
-import { View } from 'react-native'
-
-export function New() {
-  return (
-    <View>New</View>
-  )
-}
