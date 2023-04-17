3d14bfa97096aab8cf5eb66e5368c2095bb0fa8c
bhigoreduardo <bhigoreduardo@gmail.com>
2023-04-17 10:47:42 -0300

    :sparkles: Create header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import './styles/global.css'
 
+import { Header } from './components/Header'
+
 function App() {
   return (
-    <div>
-    </div>
+    <main className="flex justify-center items-center w-screen h-screen bg-background">
+      <section className="flex flex-col gap-16 w-full max-w-5xl px-6">
+        <Header />
+      </section>
+    </main>
   )
 }
 
