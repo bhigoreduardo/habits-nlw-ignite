f1aa0200fc79b91a25a9141a1eedfabb8dfafbb1
bhigoreduardo <bhigoreduardo@gmail.com>
2023-04-17 19:43:44 -0300

    :white_check_mark: Get and toggle habit list

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,56 @@
-export function HabitDay() {
+import { useState } from 'react'
+import * as Popover from '@radix-ui/react-popover'
+import clsx from 'clsx'
+import dayjs from 'dayjs'
+
+import { HabitsList } from './HabitsList'
+import { Progress } from './Progress'
+
+interface HabitDayProps {
+  date: Date,
+  defaultCompleted?: number,
+  amount?: number,
+}
+
+export function HabitDay({ date, defaultCompleted = 0, amount = 0 }: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted)
+
+  const progress = amount > 0 ? Math.round(completed / amount * 100) : 0
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today)
+
+  const dayOfWeek = dayjs(date).format('dddd')
+  const dayAndMonth = dayjs(date).format('DD/MM')
+
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed)
+  }
+
   return (
-    <span className="flex items-center justify-center h-10 w-10 border-2 rounded-lg bg-violet-900 border-violet-700">
-    </span>
+    <Popover.Root>
+      <Popover.Trigger
+        className={clsx('flex items-center justify-center h-10 w-10 border-2 rounded-lg', {
+          'bg-zinc-900 border-zinc-800': progress === 0,
+          'bg-violet-900 border-violet-700': progress > 0 && progress < 20,
+          'bg-violet-800 border-violet-600': progress >= 20 && progress < 40,
+          'bg-violet-700 border-violet-500': progress >= 40 && progress < 60,
+          'bg-violet-600 border-violet-400': progress >= 60 && progress < 80,
+          'bg-violet-500 border-violet-300': progress >= 80,
+          'border-white border-2': isCurrentDay,
+        })}
+      />
+
+      <Popover.Portal>
+        <Popover.Content className="flex flex-col min-w-[320px] p-6 rounded-2xl bg-zinc-900">
+          <span className="font-semibold text-zinc-400 text-base lowercase">{dayOfWeek}</span>
+          <span className="font-extrabold text-white text-3xl leading-tight mt-2">{dayAndMonth}</span>
+
+          <Progress progress={progress} />
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
+
+          <Popover.Arrow width={16} height={8} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
+
+import { api } from '../lib/axios'
 import { weekDays, amountOfDaysToFill } from '../utils'
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 
@@ -5,7 +9,22 @@ import { HabitDay } from './HabitDay'
 
 const summaryDates = generateDatesFromYearBeginning()
 
+type Summary = Array<{
+  id: string,
+  date: string,
+  completed: number,
+  amount: number,
+}>
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() => {
+    api.get('/summary').then((res) => {
+      setSummary(res.data)
+    })
+  }, [])
+
   return (
     <article className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -20,17 +39,28 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {
-          summaryDates.map((item) => (
-            <HabitDay
-              key={item.toString()}
-            />
-          ))
+          summary.length > 0 && summaryDates.map((item) => {
+            const dayInSummary = summary.find((day) => {
+              return dayjs(item).isSame(day.date, 'day')
+            })
+
+            return (
+              <HabitDay
+                key={item.toString()}
+                date={item}
+                defaultCompleted={dayInSummary?.completed}
+                amount={dayInSummary?.amount}
+              />
+            )
+          })
         }
         {
           amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => (
-            <HabitDay
+            <span
               key={i}
-            />
+              className="flex items-center justify-center h-10 w-10 border-2 rounded-lg bg-zinc-900 border-zinc-800 cursor-not-allowed opacity-40"
+            >
+            </span>
           ))
         }
       </div>
